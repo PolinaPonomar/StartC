ea6dd3a02d1232520c141e054c2ba72e86c2fc48
natakhatina <natakhatina@gmail.com>
2018-09-07 17:37:25 +0300

    result

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int a = 7;
+   // float b = 6.7;
+    std::cout << a*3<<std::endl;
+    //a=a*3;
+    std::cout << a/3<<std::endl;
+    std::cout << a+3<<std::endl;
+    std::cout << a-3<<std::endl;
+    return 0;
+}
