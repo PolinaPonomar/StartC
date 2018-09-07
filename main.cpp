eecf95f4e0c9543e3293ddea7ab52b5e10961d0f
natakhatina <natakhatina@gmail.com>
2018-09-07 17:49:53 +0300

    char

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 
 int main() {
-    int a = 7;
-   // float b = 6.7;
-    std::cout << a*3<<std::endl;
-    //a=a*3;
-    std::cout << a/3<<std::endl;
-    std::cout << a+3<<std::endl;
-    std::cout << a-3<<std::endl;
+   // int a = 7;
+    // float b = 6.7;
+    char c = 125;
+    std::cout << c;
     return 0;
 }
