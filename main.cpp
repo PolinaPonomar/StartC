508fe17da7544a2f8191e2df848ecba51bed047c
natakhatina <natakhatina@gmail.com>
2018-09-07 17:55:40 +0300

    charcharchar

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@ int main() {
    // int a = 7;
     // float b = 6.7;
     char c = 125;
-    std::cout << c;
+    std::cout << c*3<<std::endl;
+    std::cout << c/3<<std::endl;
+    std::cout << c+3<<std::endl;
+    std::cout << c-3<<std::endl;
+    std::cout << c<<std::endl;
+    std::cout << char(c*3)<<std::endl;
+    std::cout << char(c/3)<<std::endl;
+    std::cout <<char( c+3)<<std::endl;
+    std::cout << char(c-3)<<std::endl;
     return 0;
 }
